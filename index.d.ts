aff07225de0c28bc1786e9a96586ab8ea9f29068
gcoro <giulia994@ymail.com>
2020-04-03 16:35:47 +0200

    Added TS types definition

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+declare module 'geojson-bounds' {
+
+	import geojson from 'geojson';
+
+	/**
+ 	* Returns an array of coordinates in the order [West, South, East, North] that represents the extent of the provided feature or geometry
+ 	*/
+	export function extent(geojson: geojson.GeoJSON): Array<number>;
+
+	/**
+ 	* Returns a GeoJSON Feature polygon that repesents the bounding box of the provided feature or geometry
+ 	*/
+	export function envelope(geojson: geojson.GeoJSON): geojson.Feature<geojson.Polygon>
+
+	/**
+ 	* Returns the western-most longitude of the provided feature or geometry
+ 	*/
+	export function xMin(geojson: geojson.GeoJSON): number;
+
+	/**
+ 	* Returns the eastern-most longitude of the provided feature or geometry
+ 	*/
+	export function xMax(geojson: geojson.GeoJSON): number;
+
+	/**
+ 	* Returns the southern-most latitude of the provided feature or geometry
+ 	*/
+	export function yMin(geojson: geojson.GeoJSON): number;
+
+	/**
+ 	* Returns the northern-most latitude of the provided feature or geometry
+ 	*/
+	export function yMax(geojson: geojson.GeoJSON): number;
+
+}
